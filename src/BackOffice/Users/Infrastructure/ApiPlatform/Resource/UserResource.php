2f88b3eb01f98b4ec71a5e92353835a4fe6de94a
Guillermo Lengemann Garcés <guillermo.lengemann@gmail.com>
2024-02-25 12:55:28 -0500

    Fix OpenAPI doc for User entity

--- src/BackOffice/Users/Infrastructure/ApiPlatform/Resource/UserResource.php
+++ src/BackOffice/Users/Infrastructure/ApiPlatform/Resource/UserResource.php
@@ -2,32 +2,34 @@
 
 namespace App\BackOffice\Users\Infrastructure\ApiPlatform\Resource;
 
-use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\ApiProperty;
 use ApiPlatform\Metadata\Post;
-use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Serializer\Attribute\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
 
-// TODO
-#[ApiResource(
-    shortName: 'User',
-    operations: [
-        new Post(
-            uriTemplate: '/login',
-            denormalizationContext: [
-                'groups' => ['user:login'],
-            ],
-        ),
-        new Post(
-            uriTemplate: '/logout',
-        ),
+#[Post(
+    routeName: 'app_login',
+    shortName: 'login',
+    denormalizationContext: [
+        'groups' => ['user:login'],
+    ],
+)]
+#[Post(
+    routeName: 'app_logout',
+    shortName: 'logout',
+    denormalizationContext: [
+        'groups' => ['user:logout'],
     ],
 )]
 final class UserResource
 {
     public function __construct(
+        #[ApiProperty(default: 'admin@demo.com')]
         #[Assert\NotBlank]
         #[Groups(['user:login'])]
         public ?string $email = null,
+
+        #[ApiProperty(default: 'password')]
         #[Assert\NotBlank]
         #[Groups(['user:login'])]
         public ?string $password = null,
